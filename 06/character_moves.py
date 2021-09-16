48787ae33242f5babde799b91c2e7c9892152afa
kangk57 <kangk5712@gmail.com>
2021-09-16 20:21:46 +0900

    DRILL #06 완성
    2020180001 강현준

--- 06/character_moves.py
+++ 06/character_moves.py
@@ -6,10 +6,12 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+
 #사각형 운동
 
 x = 400
 y = 90
+ang = 0
 while (True):
     clear_canvas_now()
     grass.draw_now(400, 30)
@@ -24,7 +26,17 @@ while (True):
         y -= 2
     delay(0.005)
 
-#원
+#원 운동
+
+    if(x == 400 and y == 90):
+        for i in range(0 , 359+1):
+            sin = math.sin(ang / 360 * 2 * math.pi)
+            cos = math.cos(ang / 360 * 2 * math.pi)
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(400-255*sin,345-255*cos)
+            ang +=1
+            delay(0.005)
 
     
 
