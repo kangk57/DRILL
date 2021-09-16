a500d9b1e033d7794ad0edb614b34eb85eb100a8
kangk57 <kangk5712@gmail.com>
2021-09-16 19:24:37 +0900

    DRILL #06 완성
    2020180001

--- 06/character_moves.py
+++ 06/character_moves.py
@@ -8,6 +8,21 @@ character = load_image('character.png')
 
 #사각형 운동
 
+x = 400
+y = 90
+while (True):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x,y)
+    if(x < 800 and y == 90):
+        x += 2
+    if(x == 800 and y < 600):
+        y += 2
+    if(x > 0 and y == 600):
+        x -= 2
+    if(x == 0 and y > 0 ):
+        y -= 2
+    delay(0.005)
 
 #원
 
