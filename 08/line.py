1a2e4baa0623985b836a88569da1161a6b38bc7b
kangk57 <kangk5712@gmail.com>
2021-10-07 18:27:48 +0900

    DRILL #08 완성
    2020180001 강현준
    https://www.youtube.com/watch?v=xWuXs84orUY
    
    미완성입니다..제출합니다..

--- 08/line.py
+++ 08/line.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+from math import cos, sin, radians
 
 
 def stop():
@@ -41,7 +42,7 @@ def prepare_turtle_canvas():
 def draw_big_point(p):
     turtle.goto(p)
     turtle.color(0.8, 0.9, 0)
-    turtle.dot(15)
+    turtle.dot(20)
     turtle.write('     '+str(p))
 
 
@@ -50,18 +51,29 @@ def draw_point(p):
     turtle.dot(5, random.random(), random.random(), random.random())
 
 
-def draw_line_basic(p1, p2):
-    # fill here
-    pass
+# def draw_line_basic(p1, p2):
+#     draw_big_point(p1)
+#     draw_big_point(p2)
+#
+#     x1, y1 = p1[0], p1[1]
+#     x2, y2 = p2[0], p2[1]
 
 
-def draw_line(p1, p2):
-    # fill here
-    pass
+def draw_line(a, b):
+    # draw_big_point(a)
+    # draw_big_point(b)
+    for i in range(0, 100 + 1, 20):
+        t = i / 100
+        x = (a[0] - b[0]) * cos(radians(t)) + b[0] * cos(t * (a[0] / b[0] - 1))
+        y = (a[1] - b[1]) * sin(radians(t)) - b[1] * sin(t * (a[1] / b[1] - 1))
+        k = a[1] / b[1]
+        draw_point((x, y))
+
+    prepare_turtle_canvas()
+    draw_line((250, 100), (250, 100))
 
 
 prepare_turtle_canvas()
-
-# fill here
+draw_line((-200, -100), (300, 150))
 
 turtle.done()
