23c67c2fbbf7ceb54cdf7d646058035aeaa9fbbe
kangk57 <kangk5712@gmail.com>
2021-11-04 20:29:20 +0900

    DRILL #11 완성
    https://www.youtube.com/watch?v=LLuS1__s2wk
    
    2020180001

--- Drill-11/boy.py
+++ Drill-11/boy.py
@@ -2,13 +2,18 @@ import grass
 from pico2d import *
 
 # Boy Event
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER = range(5)
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER, \
+ SHIFT_DOWN, SHIFT_UP, DASH_TIMER = range(8)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
     (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
+    (SDL_KEYDOWN, SDLK_RSHIFT): SHIFT_DOWN,
+    (SDL_KEYDOWN, SDLK_LSHIFT): SHIFT_DOWN,
     (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
-    (SDL_KEYUP, SDLK_LEFT): LEFT_UP
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
+    (SDL_KEYUP, SDLK_RSHIFT): SHIFT_UP,
+    (SDL_KEYUP, SDLK_LSHIFT): SHIFT_UP
 }
 
 
@@ -89,14 +94,58 @@ class SleepState:
                                           '', boy.x + 25, boy.y - 25, 100, 100)
 
 
+class DashState:
+
+    def enter(boy, event):
+        if event == RIGHT_DOWN:
+            boy.velocity += 1
+        elif event == LEFT_DOWN:
+            boy.velocity -= 1
+        elif event == RIGHT_UP:
+            boy.velocity -= 1
+        elif event == LEFT_UP:
+            boy.velocity += 1
+        # boy.dir = boy.velocity
+
+    def exit(boy, event):
+        pass
+
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x += boy.velocity * 2.5
+        boy.x = clamp(25, boy.x, 800 - 25)
+        boy.timer -= 1
+        if boy.timer == 0:
+            boy.add_event(DASH_TIMER)
+
+    def draw(boy):
+        if boy.velocity == 1:
+            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
+
 next_state_table = {
     IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState,
                 RIGHT_DOWN: RunState, LEFT_DOWN: RunState,
-                SLEEP_TIMER: SleepState},
+                SLEEP_TIMER: SleepState,
+                SHIFT_DOWN and SHIFT_DOWN: IdleState,
+                SHIFT_DOWN: IdleState, SHIFT_UP: IdleState},
+
     RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
-               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState},
+               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState,
+               SHIFT_DOWN: DashState, SHIFT_UP: RunState,
+               SHIFT_DOWN and SHIFT_DOWN: DashState},
+
     SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
-                 LEFT_UP: RunState, RIGHT_UP: RunState}
+                 LEFT_UP: RunState, RIGHT_UP: RunState,
+                 SHIFT_DOWN and SHIFT_DOWN: SleepState,
+                 SHIFT_DOWN: SleepState, SHIFT_UP: SleepState},
+
+    DashState: {RIGHT_UP: DashState, LEFT_UP: DashState,
+                RIGHT_DOWN: DashState, LEFT_DOWN: DashState,
+                DASH_TIMER: RunState,
+                SHIFT_DOWN: DashState, SHIFT_UP: RunState,
+                }
 }
 
 
